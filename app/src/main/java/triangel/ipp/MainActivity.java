582ebae178e0dfa747e25824ac87ae5c608ddffc
Takolander <william_tako@hotmail.com>
2018-04-24 18:13:08 +0200

    Second Commit

--- app/src/main/java/triangel/ipp/MainActivity.java
+++ app/src/main/java/triangel/ipp/MainActivity.java
@@ -2,12 +2,40 @@ package triangel.ipp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    public TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.t_box);
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+        reference.child("1").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String temp = dataSnapshot.getValue(String.class);
+                textView.setText(temp);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
