5295e43483de506bd3ff4479b18995894cacb855
Takolander <william_tako@hotmail.com>
2018-05-03 22:33:52 +0200

    Third Commit

--- app/src/main/java/triangel/ipp/MainActivity.java
+++ app/src/main/java/triangel/ipp/MainActivity.java
@@ -1,7 +1,19 @@
 package triangel.ipp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference reference;
     public TextView textView;
+
+    private Button button;
+    private TextView coordinates;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    public boolean permission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +55,64 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        permission = runtime_permissions();
+
+        button = (Button) findViewById(R.id.getLocation);
+        coordinates = (TextView) findViewById(R.id.locationView);
+
+        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                coordinates.append("\n "+location.getLatitude()+ " " +location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        configureButton();
+    }
+
+    private boolean runtime_permissions(){
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                configureButton();
+            }
+            runtime_permissions();
+        }
+    }
+
+    private void configureButton(){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                locationManager.requestLocationUpdates("gps", 5000, 0 ,locationListener);
+            }
+        });
+
     }
 }
